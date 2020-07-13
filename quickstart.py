fd5df486abee939e744d60d2a283049d6f02a3fd
nootmoosie <nathanjmasi@gmail.com>
2020-07-13 17:01:07 -0400

    comment of flow for downloading file

--- quickstart.py
+++ quickstart.py
@@ -53,3 +53,12 @@ def main():
 
 if __name__ == '__main__':
   main()
+
+# file_id = '0BwwA4oUTeiV1UVNwOHItT0xfa2M'
+# request = drive_service.files().get_media(fileId=file_id)
+# fh = io.BytesIO()
+# downloader = MediaIoBaseDownload(fh, request)
+# done = False
+# while done is False:
+#     status, done = downloader.next_chunk()
+#     print "Download %d%%." % int(status.progress() * 100)
